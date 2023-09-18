a0b317c1ca7cae2957a76511e3d67587c9baafe1
sahil.msdev <sahil.msdev@outlook.com>
2023-09-18 12:59:59 +0530

    1. Updated server js to handle root path 2. Removed unnecessary stuff from package.json 3. Cleaned gitignore file

--- server.js
+++ server.js
@@ -10,8 +10,17 @@ const meetingsRouter = require('./routes/meetings');
 app.use('/contacts', contactsRouter);
 app.use('/meetings', meetingsRouter);
 
+//Define a route handler for teh root path '/'
+app.get('/',(req,res) => {
+  res.send('Welcome! RMA Server is running.'); //Sends the response to the client
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
+  
   console.log(`RMA Server is running on port ${port}`);
+  const hostname = 'localhost'
+  const url = `http://${hostname}:${port}`;
+  console.log(`${url}`);
 });
